99370561ed202b372b69e36349129e07291fcab0
NindiFadhilatun <104575390+NindiFadhilatun@users.noreply.github.com>
2024-07-03 17:01:57 +0700

    read files

--- main.py
+++ main.py
@@ -1,26 +1,58 @@
 from docx.api import Document
+import os
 
-# Open the .docx file
-doc = Document('test.docx')
+source_folder = r'C:\Users\matra\PycharmProjects\AutoFileName'
 
-# Create an empty list to store all paragraphs
-paraText = ""
-tableText = ""
-# Iterate through each paragraph and append its text to the list
-for p in doc.paragraphs:
-    paraText = paraText + p.text
+for item in os.listdir(source_folder):
+    # check if an item is file or not
+    if os.path.isfile(os.path.join(source_folder, item)):
+        if item.endswith('.docx'):
+            try:
+                # Open the .docx file
+                doc = Document(item)
 
-for table in doc.tables:
-    for row in table.rows:
-        tableText = tableText + " ".join([cell.text for cell in row.cells])
+                # Create an empty list to store all paragraphs
+                paraText = ""
+                tableText = ""
 
-fileName = (tableText + paraText).replace("\n", " ")
-# remove double space
-fileName = ' '.join(fileName.split())
-mid = int(len(fileName)/2)
-q1 = int(len(fileName)/4)
-q3 = int(len(fileName)*3/4)
-if len(fileName) > 190:
-    fileName = fileName[0:60] + fileName[q1:q1+30] + fileName[mid:mid+35] + fileName[q3:q3+30] + fileName[-35:]
+                # Iterate through each paragraph and append its text to the list
+                for p in doc.paragraphs:
+                    paraText = paraText + p.text
 
-print(fileName)
+                for table in doc.tables:
+                    for row in table.rows:
+                        tableText = tableText + " ".join([cell.text for cell in row.cells])
+
+                # remove forbidden ASCII characters for file name
+                fileName = ((tableText + paraText).replace("\n", " ")
+                            .replace("/", "").replace("\\", "")
+                            .replace("<", "").replace(">", "")
+                            .replace(":", "").replace(";", "")
+                            .replace("|", "").replace("?", "")
+                            .replace("&", "").replace("!", "")
+                            .replace("@", "").replace("#", "")
+                            .replace("$", "").replace("^", "")
+                            .replace("+", "").replace("-", "")
+                            .replace("~", "").replace("`", "")
+                            .replace(".", "").replace("*", ""))
+
+                # remove double space
+                fileName = ' '.join(fileName.split())
+
+                mid = int(len(fileName) / 2)
+                q1 = int(len(fileName) / 4)
+                q3 = int(len(fileName) * 3 / 4)
+
+                if len(fileName) > 190:
+                    fileName = (fileName[0:70] + fileName[q1:q1 + 30]
+                                + fileName[mid:mid + 30] + fileName[q3:q3 + 30]
+                                + fileName[-30:])
+                print(fileName)
+                os.rename(
+                    os.path.join(source_folder, item),
+                    os.path.join(source_folder, fileName + '.docx')
+                )
+            except PermissionError:
+                continue
+            except Exception as e:
+                raise Exception(e)
