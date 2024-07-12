661a5745be6d068c3d72f137870ff3a17609e685
NindiFadhilatun <104575390+NindiFadhilatun@users.noreply.github.com>
2024-07-12 12:44:44 +0700

    Clean print

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from tkinter import *
 from tkinter import filedialog
+
 from spire.doc import *
 from spire.doc.common import *
-import time
 
-start = time.time()
 root = Tk()
 root.withdraw()
 source_folder = filedialog.askdirectory()
-sorted_folder = r"C:\Users\okipr\Documents\Sort"
+sorted_folder = r"C:\Users\matra\Documents\sort"
 
 
 def filename_cleaner(target):
@@ -28,6 +27,7 @@ def filename_cleaner(target):
               .replace("direktorat", "").replace("jenderal", "")
               .replace("evaluation warning the document was created with spiredoc for python", "")
               .replace("balai pengujian dan identifikasi barang", "bpib")
+              .replace("balai laboratorium bea dan cukai", "blbc")
               .replace("balai laboratorium", "")
               .replace("kementerian keuangan", "")
               .replace("bea dan cukai", "")
@@ -45,54 +45,55 @@ def filename_cleaner(target):
               .replace("surat elektronik", "").replace("laman", "")
               .replace("bpibsurabayayahoocom", "").replace("bpibyahoocom", "")
               .replace("wwwbeacukaigoid", "")
+              .replace("seksi program dan evaluasi", "spe")
+              .replace("seksi teknis laboratorium", "stl")
+              .replace("subbagian umum dan kepatuhan internal", "sbuki")
+              .replace("sub bagian umum dan kepatuhan internal", "sbuki")
               .replace("pusat kontak layanan", "")
               .replace("1500225", "").replace("surel", "")
               .replace("bcbpibcustomsgoid", "").replace("nota dinas", "nd")
-              .replace("ii", "").replace("yth", ""))
+              .replace("tanggal", "tgl").replace("dengan", "dgn")
+              .replace("ii", "").replace("yth", "").replace("nomor", "no"))
 
     # remove double space
     target = ' '.join(target.split())
-    return target[:225]
+    return target[:215]
 
 
 def read_ms_word(target):
     # Create an object of the Document class
     document = Document()
     x = ""
-    # noinspection PyBroadException
     try:
         if target.lower().endswith(".docx"):
             document.LoadFromFile(target, FileFormat.Docx)
             x = "x"
         else:
             document.LoadFromFile(target, FileFormat.Doc)
-    except Exception:
+    except:
         if target.lower().endswith(".docx"):
             document.LoadFromFile(target, FileFormat.Docx, "iso17025")
             x = "x"
         else:
             document.LoadFromFile(target, FileFormat.Doc, "iso17025")
         document.RemoveEncryption()
+    words = document.GetText().lower()
     document.Close()
-    return os.path.join(sorted_folder, filename_cleaner(document.GetText().lower()) + '.doc' + x)
+    return filename_cleaner(words) + '.doc' + x
 
 
 for item in os.listdir(source_folder):
     # check if an item is file or not
     item_path = os.path.join(source_folder, item)
     if os.path.isfile(item_path):
-        # noinspection PyBroadException
         try:
-            updated_path = read_ms_word(item_path)
+            filename = read_ms_word(item_path)
+            updated_path = os.path.join(sorted_folder, filename)
             try:
                 os.rename(item_path, updated_path)
             except FileExistsError:
                 os.remove(updated_path)
                 os.rename(item_path, updated_path)
-            os.remove(item_path)
         except Exception as e:
             print(item_path, e)
             continue
-
-end = time.time()
-print("Program execution run time: ", end - start)
